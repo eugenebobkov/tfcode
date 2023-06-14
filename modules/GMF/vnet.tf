d3b7ed1af951e2ba9e50b559fc4dcd6eaec3cfc2
eugene bobkov <eugene.bobkov@bluecrystal.com.au>
2023-06-14 09:18:43 +0000

    vnet updated

--- modules/GMF/vnet.tf
+++ modules/GMF/vnet.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet_gmf" {
     name                = format("vnet_csenergy_gmf_%s", var.azure_region)
     address_space       = ["10.30.100.0/24"]
     location            = var.azure_region
-    resource_group_name = rg_gmf_network.name
+    resource_group_name = azurerm_resource_group.rg_gmf_network.name
 
     tags = {
       module = "GMF"
@@ -10,16 +10,12 @@ resource "azurerm_virtual_network" "vnet_gmf" {
     }
 }
 
-# Create subnet
+# create subnet
 resource "azurerm_subnet" "snet_gmf_app_services" {
-    name                 = "LabSubnet"
-    resource_group_name = "Enter Resource Group Name"
-    virtual_network_name = azurerm_virtual_network.TFNet.name
-    address_prefixes       = ["10.0.1.0/24"]
+    name                 = format("snet-csenergy-gmf-app-services-%s", var.azure_region)
+    resource_group_name  = azurerm_resource_group.rg_gmf_network.name
+    virtual_network_name = azurerm_virtual_network.vnet_gmf.name
+    address_prefixes     = ["10.30.100.0/27"]
 }
-resource "azurerm_subnet" "tfsubnet2" {
-    name                 = "LabSubnet2"
-    resource_group_name = "Enter Resource Group Name"
-    virtual_network_name = azurerm_virtual_network.TFNet.name
-    address_prefixes       = ["10.0.2.0/24"]
-}
+
+
