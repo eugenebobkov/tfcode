657c340a830eeddb21b1c5d59f97365e7f7491c2
eugene bobkov <eugene.bobkov@bluecrystal.com.au>
2023-06-14 10:08:54 +0000

    azurerm > 3

--- modules/GMF/app-services.tf
+++ modules/GMF/app-services.tf
@@ -8,21 +8,21 @@ resource "azurerm_service_plan" "asp_gmf" {
 }
 
 # Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp_gmf_middleware" {
-  name                  = "webapp-gmf-middleware"
-  location              = azurerm_resource_group.rg_gmf_app_services.location
-  resource_group_name   = azurerm_resource_group.rg_gmf_app_services.name
-  service_plan_id       = azurerm_service_plan.asp_gmf.id
-  https_only            = true
-  virtual_network_subnet_id = azurerm_subnet.snet_gmf_app_services.id
-  site_config { 
-    minimum_tls_version = "1.2"
-    application_stack {
-      java_server = "JAVA"
-      java_version = "17"
-    }
-  }
-}
+#resource "azurerm_linux_web_app" "webapp_gmf_middleware" {
+#  name                  = "webapp-gmf-middleware"
+#  location              = azurerm_resource_group.rg_gmf_app_services.location
+#  resource_group_name   = azurerm_resource_group.rg_gmf_app_services.name
+#  service_plan_id       = azurerm_service_plan.asp_gmf.id
+#  https_only            = true
+#  virtual_network_subnet_id = azurerm_subnet.snet_gmf_app_services.id
+#  site_config { 
+#    minimum_tls_version = "1.2"
+#    application_stack {
+#      java_server = "JAVA"
+#      java_version = "17"
+#    }
+#  }
+#}
 
 #  Deploy code from a public GitHub repo
 #resource "azurerm_app_service_source_control" "sourcecontrol" {
@@ -31,5 +31,4 @@ resource "azurerm_linux_web_app" "webapp_gmf_middleware" {
 #  branch             = "master"
 #  use_manual_integration = true
 #  use_mercurial      = false
-A
 #}
